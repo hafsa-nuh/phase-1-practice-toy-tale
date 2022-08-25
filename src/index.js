3264f14b77a178564cceb8ad9b17250fc315edf0
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-25 09:58:39 +0300

    Almost

--- src/index.js
+++ src/index.js
@@ -1,15 +1,81 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
+  const paragraphBtn = document.querySelector("#new-toy-btn");
+  const toyFormContainerDiv = document.querySelector(".container");
+  paragraphBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
-      toyFormContainer.style.display = "block";
+      toyFormContainerDiv.style.display = "block";
     } else {
-      toyFormContainer.style.display = "none";
+      toyFormContainerDiv.style.display = "none";
     }
   });
 });
+
+// Add Toy Info to the Card
+function gettingCard (toyData){
+  let card = document.createElement('div')
+  card.className = `card`
+  card.innerHTML = `
+  <h2>${toyData.name}</h2>
+  <img src="${toyData.image}" class="toy-avatar" />
+  <p>${toyData.likes} likes</p>
+  <button class="like-btn" id="[toy_id]">Like ❤️</button>
+  `
+  document.querySelector('#toy-collection').appendChild(card)
+}
+
+// fetch / GET
+function getAllToys (){
+  fetch('http://localhost:3000/toys')
+  .then (response => response.json())
+  .then (toyData => toyData.forEach(toy => gettingCard(toy)))
+}
+getAllToys()
+
+
+// Add a New Toy POST
+function addingToyPost (toyObj){
+  fetch('http://localhost:3000/toys', {
+    method : 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: "application/json"
+    },
+    body: JSON.stringify(toyObj)
+  })
+  .then(res => res.json())
+}
+addingToyPost()
+
+//Increase a Toy's Likes
+function likes(event){
+  event.preventDefault()
+  let more = parseInt(event.target.previousElementSibling.innerText) + 1
+  fetch(`http://localhost:3000/toys/${event.target.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": more
+    })
+  })
+  .then(res => res.json())
+  .then(((event) => {
+    event.target.previousElementSibling.innerText = `${more} likes`;
+  }))
+}
+// button of likes
+let btn = document.querySelector('.like-btn')
+btn.addEventListener('click', event => likes(event))
+
+
+
+
+
+
+
